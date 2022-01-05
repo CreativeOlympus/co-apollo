2c5af6882536eb063d6d39dffb287029c8aba47f
Carlos Hallan <carloshallan@gmail.com>
2022-01-04 23:44:27 -0300

    add new rules in eslint and configure prettier

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+const options = {
+  arrowParens: 'avoid',
+  singleQuote: true,
+  backetsSpacing: true,
+  enfOfLine: 'lf',
+  semi: false,
+  tabWidth: 2,
+  tailingComma: 'none',
+}
+
+module.exports = options
