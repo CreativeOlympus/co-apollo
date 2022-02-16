7f98747aff1756fd3654ca5844ced0ccf2a3afc6
= <=>
2022-02-16 09:17:05 -0300

    add heroicons as a dev dependency

--- dev/serve.vue
+++ dev/serve.vue
@@ -1,24 +1,20 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 // Uncomment import and local "components" registration if library is not registered globally.
-import { CoApolloDsSample, CoaButton, CoaListItem } from '@/entry.esm'
+import { CoaButton } from '@/entry.esm'
 
 export default defineComponent({
   name: 'ServeDev',
   components: {
-    CoApolloDsSample,
     CoaButton,
-    CoaListItem,
   },
 })
 </script>
 
 <template>
   <div id="app">
-    <co-apollo-ds-sample />
     <h1 class="text-3xl font-bold underline">Test tailwind</h1>
     <coa-button> Click here </coa-button>
-    <coa-list-item tag="ul" />
     <button class="btn btn-blue background-red">click here 2</button>
   </div>
 </template>
