02914df676626355973c4ee27168d0038b0e22a4
= <=>
2022-02-16 00:48:51 -0300

    clean test components

--- src/lib-components/index.ts
+++ src/lib-components/index.ts
@@ -1,4 +1,1 @@
-export { default as CoApolloDsSample } from './co-apollo-ds-sample.vue'
-export { default as CoaList } from './Navigation/coa-list.vue'
-export { default as CoaListItem } from './Navigation/coa-list-item.vue'
 export { default as CoaButton } from './coa-button.vue'
