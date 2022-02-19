7ccb5b0c537fc78c480d513441b5b11eb44151e9
= <=>
2022-02-19 10:54:40 -0300

    testing tailwind apply

--- src/lib-components/atoms/button/coa-button.vue
+++ src/lib-components/atoms/button/coa-button.vue
@@ -16,3 +16,17 @@ export default /*#__PURE__*/ defineComponent({
     <slot />
   </button>
 </template>
+
+<style scoped>
+.btn {
+  @apply font-bold py-2 px-4 rounded;
+}
+
+.btn-blue {
+  @apply bg-blue-500 text-white;
+}
+
+.btn-blue:hover {
+  @apply bg-blue-700;
+}
+</style>
