35ff45c642abf6465340b03cdf274c8b59cb1e93
= <=>
2022-03-14 02:05:53 -0300

    button component

--- src/lib-components/atoms/button/coa-button.vue
+++ src/lib-components/atoms/button/coa-button.vue
@@ -1,32 +1,58 @@
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 
 export default /*#__PURE__*/ defineComponent({
   name: 'Button',
+  props: {
+    color: {
+      type: String,
+      default: 'blue',
+    },
+    rounded: {
+      type: Boolean,
+      default: false,
+    },
+    type: {
+      type: String,
+      default: 'normal',
+    },
+    size: {
+      type: String,
+      default: 'base',
+    },
+    disabled: Boolean,
+    fullWidth: Boolean,
+  },
+  setup(props) {
+    const buttonType = ref(props.type)
+    const allClasses = ref({
+      'coa-btn': true,
+      [`coa-btn-${props.color}`]: true,
+      [`coa-btn-${props.type}`]: true,
+      [`coa-btn-${props.size}`]: true,
+      fullWidth: props.fullWidth,
+      rounded: props.rounded,
+      disabled: props.disabled,
+    })
+
+    return { allClasses, buttonType }
+  },
   methods: {
-    handleClick() {
-      console.log('clicked')
+    handleClick(event: HTMLElement) {
+      this.$emit('click', event)
     },
   },
 })
 </script>
 
 <template>
-  <button class="btn btn-blue" @click="handleClick">
+  <button :class="allClasses" @click="handleClick" :disabled="disabled">
+    <slot name="prepend"></slot>
     <slot />
+    <slot name="append"></slot>
   </button>
 </template>
 
 <style scoped>
-.btn {
-  @apply font-bold py-2 px-4 rounded;
-}
-
-.btn-blue {
-  @apply bg-blue-500 text-white;
-}
-
-.btn-blue:hover {
-  @apply bg-blue-700;
-}
+@import './style.css';
 </style>
--- src/lib-components/atoms/button/types.ts
+++ src/lib-components/atoms/button/types.ts
@@ -1,3 +1,17 @@
 export interface ButtonText {
   text: string
 }
+
+export type ButtonType = {
+  normal: 'normal'
+  success: 'success'
+  delete: 'delete'
+  cancel: 'cancel'
+}
+
+export type ButtonSize = {
+  base: 'base'
+  xs: 'xs'
+  sm: 'sm'
+  lg: 'lg'
+}
